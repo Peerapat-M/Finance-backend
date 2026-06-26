23ecf888dc44fbe7701839fdab2dddea4ea7024f
Peerapat Moolmanee <peerapat@sobtid.me>
2026-06-26 13:54:08 +0700

    add start script

--- server.ts
+++ server.ts
@@ -92,6 +92,6 @@ app.delete('/api/transactions/:id', authenticateUser, async (req: Request, res: 
   res.json({ message: 'Transaction deleted successfully' });
 });
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
